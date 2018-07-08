53a47999c236979ea3e806de9254187daf503035
Steven Daut <stmada@gmail.com>
2018-07-08 16:17:09 -0500

    changing python sql queries

--- log.py
+++ log.py
@@ -16,11 +16,13 @@ def connect(dbname="news"):
 def pop_art():
     """Prints most popular three articles."""
     db, c = connect()
-    query = "select * from popular_articles limit 3"
+    query = "select title, views from (select title, count(title) as views from\
+    articles, log where log.path = concat('/article/',articles.slug) group by\
+    title order by views desc) as most_popular limit 3;"
     c.execute(query)
     result = c.fetchall()
     db.close()
-    print("\nPopular Articles:\n")
+    print("\n###Popular Articles###\n")
     for i in result:
         print("\"" + i[0] + "\"" + " -- " + str(i[1]) + " views")
 
@@ -28,11 +30,14 @@ def pop_art():
 def pop_auth():
     """Prints most popular article authors of all time."""
     db, c = connect()
-    query = "select * from popular_authors"
+    query = "select name, views from (select authors.name, \
+    count(articles.author) as views from articles, log, authors where \
+    log.path = concat('/article/',articles.slug) and articles.author = \
+    authors.id group by authors.name order by views desc) as pop_authors;"
     c.execute(query)
     result = c.fetchall()
     db.close()
-    print("\nPopular Authors:\n")
+    print("\n###Popular Authors###\n")
     for i in result:
         print("\"" + i[0] + "\"" + " -- " + str(i[1]) + " views")
 
@@ -40,11 +45,15 @@ def pop_auth():
 def log_err():
     """Print days on which more than 1% of browser requests lead to errors"""
     conn, c = connect()
-    query = "select * from log_errors where error_percent > 1.0"
+    query = "select date, error_percent from \
+    (select date(time),round(100.0*sum(case log.status when '200 OK' then \
+    0 else 1 end)/count(log.status),2) as error_percent from log group \
+    by date(time) order by error_percent desc) as log_errors where\
+    error_percent > 1.0;"
     c.execute(query)
     result = c.fetchall()
     conn.close()
-    print("\nDays with more than 1% of errors:\n")
+    print("\n###Days with more than 1% of errors###\n")
     for i in result:
         print(i[0].strftime('%B %d, %Y') + ' -- ' + str(i[1]) + '% errors')
 
